9f45d1acef0e8346730d2b038b5cf013cacaba9e
hidaya <hidaya.regragui@gmail.com>
2023-12-20 17:03:44 +0100

    aiarbnb v 2

--- console.py
+++ console.py
@@ -115,33 +115,34 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, args):
         """ Create an object of any class with parameters"""
+        args = args.split()
         if not args:
             print("** class name missing **")
             return
-        params = args.split(' ')
-        if params[0] not in HBNBCommand.classes:
+        if args[0] not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
 
-        new_instance = HBNBCommand.classes[params[0]]()
-        for arg in params[1:]:
-            key, value = arg.split('=')
-            key = key.replace('_', ' ').strip()
-            value = value.replace('_', ' ').strip()
+        new_instance = HBNBCommand.classes[args[0]]()
+        for arg in args[1:]:
+            if '=' in arg:
+                key, value = arg.split('=')
+                key = key.replace('_', ' ').strip()
+                value = value.replace('_', ' ').strip()
 
-            if value.startswith('"') and value.endswith('"'):
-                value = value[1:-1].replace('\\"', '"')
-            else:
-                try:
-                    if '.' in value:
-                        value = float(value)
-                    else:
-                        value = int(value)
-                except ValueError:
-                    print(f"** Value error for {value}")
-                    continue
-            if hasattr(new_instance, key):
-                setattr(new_instance, key, value)
+                if value.startswith('"') and value.endswith('"'):
+                    value = value[1:-1].replace('\\"', '"')
+                else:
+                    try:
+                        if '.' in value:
+                            value = float(value)
+                        else:
+                            value = int(value)
+                    except ValueError:
+                        print(f"** Value error for {value}")
+                        continue
+                if hasattr(new_instance, key):
+                    setattr(new_instance, key, value)
         storage.new(new_instance)
         print(new_instance.id)
         new_instance.save()
