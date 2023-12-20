378d8a5e192cf7b57f6a80d00e801ba5f4df82a5
hidaya <hidaya.regragui@gmail.com>
2023-12-20 18:23:08 +0100

    aiarbnb v 2

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,8 +8,14 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
+        if cls is not None:
+            file_storage = {}
+            for k, v in self.__objects.items():
+                if cls == v.__class__ or cls == v.__class__.__name__:
+                    file_storage[k] = v
+            return file_sorage
         return FileStorage.__objects
 
     def new(self, obj):
@@ -48,3 +54,10 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """Deletes obj from __objects if it exists"""
+        if obj:
+            k = obj.__class__.__name__ + "." + obj.id
+            if k in self.__objects:
+                del self.__objects[k]
